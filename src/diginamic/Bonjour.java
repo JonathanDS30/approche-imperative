3dac33d97055152006ffdbe0b1c00db33f16d675
JonathanDS30 <jonathan.dellasantina433@gmail.com>
2022-10-06 15:44:03 +0200

    init

---
+++ src/diginamic/Bonjour.java
@@ -0,0 +1,13 @@
+package diginamic;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		int i = 3;
+		i= i + 2;
+		String Hi = "bonjour"; 
+		System.out.println(i);
+		System.out.println(Hi + i);
+	}
+
+}
