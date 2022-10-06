3b4c4f94bfd5bd9675ed678a9c69d651476d1a59
JonathanDS30 <jonathan.dellasantina433@gmail.com>
2022-10-06 16:40:17 +0200

    Tp3

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,18 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte b = 10;
+		short s = -1782;
+		int i = 255;
+		long l = 10000L;
+		float f = 2.5F;
+		double d = 6.4D;
+		char c = 'c';
+		boolean myBool = true;
+		String randomString = "Voici le résultat d'un calcul : 1+5=6";
+		System.out.println(randomString);
+	}
+
+}
